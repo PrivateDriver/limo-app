01ded2647ffadfb866e70daee7b3c6265dfd2cfd
Jeff Webb <webbwear@gmail>
2024-08-23 10:39:49 -0400

    migrate join table Driver

--- db/migrate/20220706180512_create_join_table_company_driver.rb
+++ db/migrate/20220706180512_create_join_table_company_driver.rb
@@ -2,7 +2,7 @@ class CreateJoinTableCompanyDriver < ActiveRecord::Migration[7.0]
   def change
     create_join_table :companies, :drivers do |t|
       t.index [:company_id, :driver_id]
-      # t.index [:driver_id, :company_id]
+      t.index [:driver_id, :company_id]
     end
   end
 end
